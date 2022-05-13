c18558525f612fad0b3c2a78410a36dccbe7194b
Yaël Guilloux <yael.guilloux@gmail.com>
2022-05-13 14:44:37 +0200

    fix(cors): add headers

--- server/api/parse.ts
+++ server/api/parse.ts
@@ -3,6 +3,9 @@ import { parseContent } from '#content/server'
 
 export default defineEventHandler(async (event) => {
   appendHeader(event, 'Access-Control-Allow-Origin', '*')
+  appendHeader(event, 'Access-Control-Allow-Credentials', 'true')
+  appendHeader(event, 'Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  
   const body = await useBody(event)
 
   // @ts-ignore
